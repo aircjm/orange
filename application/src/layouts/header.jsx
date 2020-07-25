cd535890f19f13a76f48846d531dfdb42fcc0477
aircjm <aircjm@gmail.com>
2020-07-25 20:37:14 +0800

    feat(删除个人信息): 删除个人信息

--- application/src/layouts/header.jsx
+++ application/src/layouts/header.jsx
@@ -9,7 +9,6 @@ import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 
 import Logo from "../logo.png"
-import WeChat from "../assets/wechat.jpg"
 
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import CameraIcon from '@material-ui/icons/Camera';
@@ -82,40 +81,35 @@ const Header = (props) => {
         isDesktop &&
         <Right>
           <Tooltip title="个人主页">
-            <a target='_blank' rel='noopener noreferrer' href='https://zhangyangyuan.com'>
+            <a target='_blank' rel='noopener noreferrer' href='https://chenjiaming.org'>
               <Icon name="home"/>
             </a>
           </Tooltip>
           <Tooltip title="电子邮箱">
-            <a target='_blank' rel='noopener noreferrer' href='mailto:zhangyue1936@gmail.com'>
+            <a target='_blank' rel='noopener noreferrer' href='mailto:aircjm@gmail.com'>
               <Icon name="at"/>
             </a>
           </Tooltip>
-          <Tooltip title="微信">
-            <a target='_blank' rel='noopener noreferrer' href={WeChat}>
-              <Icon name="weixin"/>
-            </a>
-          </Tooltip>
-          <Tooltip title="Facebook">
-            <a target='_blank' rel='noopener noreferrer' href='https://www.facebook.com/Jansoracom'>
-              <Icon name="facebook-square"/>
-            </a>
-          </Tooltip>
-          <Tooltip title="Twitter">
-            <a target='_blank' rel='noopener noreferrer' href='https://twitter.com/_Jansora'>
-              <Icon name="twitter-square"/>
-            </a>
-          </Tooltip>
+          {/*<Tooltip title="Facebook">*/}
+            {/*<a target='_blank' rel='noopener noreferrer' href='https://www.facebook.com/Jansoracom'>*/}
+              {/*<Icon name="facebook-square"/>*/}
+            {/*</a>*/}
+          {/*</Tooltip>*/}
+          {/*<Tooltip title="Twitter">*/}
+            {/*<a target='_blank' rel='noopener noreferrer' href='https://twitter.com/_Jansora'>*/}
+              {/*<Icon name="twitter-square"/>*/}
+            {/*</a>*/}
+          {/*</Tooltip>*/}
           <Tooltip title="Github">
-            <a target='_blank' rel='noopener noreferrer' href='https://github.com/Jansora'>
+            <a target='_blank' rel='noopener noreferrer' href='https://github.com/aircjm'>
               <Icon name="github"/>
             </a>
           </Tooltip>
-          <Tooltip title="Gitter">
-            <a target='_blank' rel='noopener noreferrer' href='https://gitter.im/Jansora/community'>
-              <Icon name="comments"/>
-            </a>
-          </Tooltip>
+          {/*<Tooltip title="Gitter">*/}
+            {/*<a target='_blank' rel='noopener noreferrer' href='https://gitter.im/Jansora/community'>*/}
+              {/*<Icon name="comments"/>*/}
+            {/*</a>*/}
+          {/*</Tooltip>*/}
         </Right>
       }
       {
